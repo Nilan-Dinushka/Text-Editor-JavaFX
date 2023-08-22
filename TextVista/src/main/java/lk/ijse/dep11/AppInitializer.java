e7c07c3af922c855b5b3b9cebf4601f1ff79a613
Nilan Dinushka <nilandinushka5@gmail.com>
2023-08-22 07:27:53 +0530

    Splash Screen Compleded

--- TextVista/src/main/java/lk/ijse/dep11/AppInitializer.java
+++ TextVista/src/main/java/lk/ijse/dep11/AppInitializer.java
@@ -1,10 +1,16 @@
 package lk.ijse.dep11;
 
+import javafx.animation.PauseTransition;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
 
 public class AppInitializer extends Application {
 
@@ -14,13 +20,40 @@ public class AppInitializer extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        AnchorPane root = FXMLLoader.load(getClass().getResource("/view/MainForm.fxml"));
-        Scene mainScene = new Scene(root);
+        Parent splashRoot = FXMLLoader.load(getClass().getResource("/view/SceneLoading.fxml"));
+        Scene splashScene = new Scene(splashRoot);
 
-        primaryStage.setScene(mainScene);
-        primaryStage.setTitle("Text Vista");
-        primaryStage.centerOnScreen();
-        primaryStage.show();
+        // Create an undecorated stage for splash screen
+        Stage splashStage = new Stage();
+        //splashStage.initStyle(StageStyle.UNDECORATED);
+        splashStage.initStyle(StageStyle.TRANSPARENT);
+        //splashRoot.setBackground(Background.fill(Color.TRANSPARENT));
+        splashScene.setFill(Color.TRANSPARENT);
+        splashStage.setScene(splashScene);
+        splashStage.show();
+
+        // Set up a pause transition for splash screen duration
+        PauseTransition pause = new PauseTransition(Duration.seconds(2));
+        pause.setOnFinished(event -> {
+            try {
+                // Close the splash screen stage
+                splashStage.close();
+
+                // Load main window
+                Parent mainRoot = FXMLLoader.load(getClass().getResource("/view/MainForm.fxml"));
+                Scene mainScene = new Scene(mainRoot);
+
+                // Create a decorated stage for the main window
+                Stage mainStage = new Stage();
+                mainStage.setScene(mainScene);
+                mainStage.setTitle("Test Vista");
+                mainStage.show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        pause.play();
+
 
     }
 }
--- TextVista/src/main/java/lk/ijse/dep11/MainFormController.java
+++ TextVista/src/main/java/lk/ijse/dep11/MainFormController.java
@@ -1,4 +1,47 @@
 package lk.ijse.dep11;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class MainFormController {
+    public AnchorPane root;
+    public MenuItem mnNew;
+    public MenuItem mnExit;
+    public MenuItem mnUserGuide;
+    public MenuItem mnAboutUs;
+
+    public void mnNewOnAction(ActionEvent actionEvent) throws Exception{
+        try {
+            // Load the new scene's FXML
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/MainForm.fxml"));
+            Parent newSceneRoot = loader.load();
+
+            // Get the stage from the menu item's context
+            Stage currentStage = (Stage) mnNew.getParentPopup().getOwnerWindow();
+
+            // Set the new scene
+            Scene newScene = new Scene(newSceneRoot);
+            currentStage.setScene(newScene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void mnExitOnAction(ActionEvent actionEvent) {
+        System.exit(0);
+    }
+
+    public void mnUserGuideOnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnAboutUsOnAction(ActionEvent actionEvent) {
+    }
 }
